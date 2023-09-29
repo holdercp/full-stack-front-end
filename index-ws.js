dbdd6c34ed3adace66271a1324e6e63af0c1ab2e
Chris Holder <chris.holder@ramseysolutions.com>
2023-09-29 10:38:23 -0500

    Add db

--- index-ws.js
+++ index-ws.js
@@ -13,6 +13,15 @@ server.listen(PORT, function () {
   console.log(`server started on port ${PORT}`);
 });
 
+process.on("SIGINT", function interceptSigInt() {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(function closeServer() {
+    shutdownDb();
+  });
+});
+
 /** Begin websockets */
 const WebSocketServer = require("ws").Server;
 
@@ -28,6 +37,10 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome to my server");
   }
 
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES(${numClients}, datetime('now'))
+  `);
+
   ws.on("close", function close() {
     wss.broadcast(`Current visitors: ${numClients}`);
     console.log("A client has disconnected");
@@ -39,3 +52,28 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+/** end websocket */
+/** begin database */
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(function serialize() {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", function each(_err, row) {
+    console.log(row);
+  });
+}
+
+function shutdownDb() {
+  getCounts();
+  console.log("shutting down db...");
+  db.close();
+}
